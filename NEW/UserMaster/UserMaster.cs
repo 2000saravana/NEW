d75bfb9cd373ddaa1b513ec6b03f6f05035eaf44
Saravana Kumar <gsaravanakumar@ebms.co.in>
2022-09-27 00:26:37 +0530

    UserMaster Added

---
+++ NEW/UserMaster/UserMaster.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NEW.UserMaster
+{
+    public class UserMaster 
+    {
+
+        public int UserMasterAutoId { get; set; }
+        public string UserName { get; set; }    
+
+        public string IndentityNo { get; set; }
+        public string Address { get; set; }
+
+        public int ContactNo { get; set; }
+        public string UserType { get; set; }
+        public char ActiveStatus { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public Guid UniqueId { get; set; }
+    } 
+}
