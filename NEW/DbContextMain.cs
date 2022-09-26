a3b7ff1dc2ef952bb90935f76f6993633245f51d
Saravana Kumar <gsaravanakumar@ebms.co.in>
2022-09-26 23:29:17 +0530

    DbContext Added

---
+++ NEW/DbContextMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace NEW
+{
+    public class DbContextMain : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=TestDB;User Id=postgres;Password=saravana;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
